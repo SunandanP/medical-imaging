9c42c53eaac449116bee0f6df4fb68fe3ae94926
Sunandan Phalke <sunandan.phalke@elastic.run>
2025-03-01 02:33:33 +0530

    Removed redundant files

--- medical_imaging/medical_imaging/doctype/patient_/test_patient_.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, algo-rhythm.tech and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestPatient_(FrappeTestCase):
-	pass
